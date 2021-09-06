f90087a92fad15c988e04d76558503b86a2fcc00
agoscervio <acervio@hotmail.com>
2021-09-06 17:17:58 -0300

    cambio fetch

--- proyecto-react/src/components/Card/Card.js
+++ proyecto-react/src/components/Card/Card.js
@@ -5,22 +5,10 @@ class Card extends Component {
     constructor(){
         super()
         this.state ={
-            movies:''
+            valor:''
         }
     }
-    componentDidMount(){
-        let url = "https://api.themoviedb.org/3/movie/popular?api_key=6137a481959516e193831c9b950f5155&language=es&page=1"
-        fetch(url)
-        .then(response => response.json())
-        .then(data =>{
-            console.log(data)
-            this.setState({
-                movies: data.results
-            })
-            
-        })
-        .catch(error => console.log(error))
-    }
+   
 
     render(){
         return(
