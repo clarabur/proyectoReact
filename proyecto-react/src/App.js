e831889a1e9b1a28145dc7eba5a1076256e92949
agoscervio <acervio@hotmail.com>
2021-09-06 16:57:35 -0300

    footer

--- proyecto-react/src/App.js
+++ proyecto-react/src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from './components/Header/Header'
 import Cards from "./components/Cards/Cards"
+import Footer from "./components/Footer/Footer"
 function App(){
     return(
         <div>
             <Header />
             <Cards/>
+            <Footer/>
         </div>
     )
 }
---
+++ proyecto-react/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Footer(){
+    return(
+        <ul class="team">
+            <li>Clara Bur</li>
+            <li>Victoria Villarreal</li>
+            <li>Agostina Cervio</li>
+        </ul>
+    )
+}
+export default Footer
