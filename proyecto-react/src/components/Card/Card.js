1cf52e0e1286ba2e17c1493d83ca67ae545733ff
Victoria Villarreal <victoriavillarreal@MacBook-Pro-de-Victoria.local>
2021-09-09 10:21:48 -0300

    parte css card

--- proyecto-react/src/components/Card/Card.js
+++ proyecto-react/src/components/Card/Card.js
@@ -28,7 +28,7 @@ class Card extends Component {
     render(){
         return(
 
-            <article>
+            <article className='card'>
             <section className="navigation">
                 <div>
                     <i className="fas fa-chevron-left"></i>
@@ -36,17 +36,21 @@ class Card extends Component {
                 </div>
                 <i className="far fa-window-close" onClick={() => this.props.remove(this.props.dataMovie.id)}></i>
             </section>
-            <main>
-                <img className="imagenes" src={`https://image.tmdb.org/t/p/w342${this.props.dataMovie.poster_path}`} alt=""/>
-                <h3>{this.props.dataMovie.title}</h3>
-                <p className="description">{this.props.dataMovie.overview}</p>
-                <section className="aditional-info">
-                    <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Fecha de lanzamiento: {this.props.dataMovie.release_date}</p>
-                    <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Título original: {this.props.dataMovie.original_title}</p>
-                    <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Lenguaje original: {this.props.dataMovie.original_language}</p>
-                    <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Popularidad: {this.props.dataMovie.popularity}</p>
-                    <p className='more' onClick={() => this.viewMore()}>{this.state.text}</p>
-                </section>
+            <main className='imagen-info'>
+                <div className='contenedor-imagen'>
+                    <img className="imagenes" src={`https://image.tmdb.org/t/p/w342${this.props.dataMovie.poster_path}`} alt=""/>
+                </div>
+                <div className='contenedor-info'>
+                    <h3>{this.props.dataMovie.title}</h3>
+                    <p className="description">{this.props.dataMovie.overview}</p>
+                    <section className="aditional-info">
+                        <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Fecha de lanzamiento: {this.props.dataMovie.release_date}</p>
+                        <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Título original: {this.props.dataMovie.original_title}</p>
+                        <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Lenguaje original: {this.props.dataMovie.original_language}</p>
+                        <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Popularidad: {this.props.dataMovie.popularity}</p>
+                        <p className='more' onClick={() => this.viewMore()}>{this.state.text}</p>
+                    </section>
+                </div>
             </main>
         </article>
 
