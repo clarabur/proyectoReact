375a2283e06e15c1cca51178525d17ea26838d76
agoscervio <acervio@hotmail.com>
2021-09-09 00:29:11 -0300

    buscador

--- proyecto-react/src/components/Header/Header.js
+++ proyecto-react/src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react' 
-import Buscador from '../Buscador/Buscador'
+
 
 function Header(){
     return(
         <div>
             <h1>Título/ Nombre de la app</h1>
-            <Buscador />
+          
         </div>
     )
 }
