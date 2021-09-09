1e773177e339897a76f05b05fe235e8d92a6b565
agoscervio <acervio@hotmail.com>
2021-09-09 09:35:45 -0300

    iconos

--- proyecto-react/src/components/Buscador/Buscador.js
+++ proyecto-react/src/components/Buscador/Buscador.js
@@ -21,10 +21,17 @@ class Buscador extends Component{
     }
     render(){
         return(
+            <section>
+            <p>Ordenar ASC/ DESC</p>
+            <i className="fas fa-th"></i>
+            <i className="fas fa-align-justify"></i>
+       
+   
             <form onSubmit={(event)=>this.evitarSubmit(event)} action="">
                 <input type="text" onChange={(event)=>this.controlarCambios(event)} value={this.state.valor} name="search" id="search" placeholder="Buscar Pelicula"/>
                 <button type="submit"><i className="fas fa-search"></i></button>
             </form>
+            </section>
         )
     }
 }
