8d3813e0d58496b843cc02f54c0ca1bd9bc2dfc9
agoscervio <acervio@hotmail.com>
2021-09-13 10:26:37 -0300

    cambio de nombre

--- proyecto-react/src/components/Buscador/Buscador.js
+++ proyecto-react/src/components/Buscador/Buscador.js
@@ -30,7 +30,7 @@ class Buscador extends Component{
    
             <form className="formContainer" onSubmit={(event)=>this.evitarSubmit(event)} action="">
                 <div className="inputDiv"><input className="input" type="text" onChange={(event)=>this.controlarCambios(event)} value={this.state.valor} name="search" id="search" placeholder="Buscar película popular por título"/></div>
-                <div className="contenedorBoton"><button className="boton" type="submit"><i className="fas fa-search icono"></i></button></div>
+                <div className="contenedorBoton"><button className="botons" type="submit"><i className="fas fa-search iconos"></i></button></div>
             </form>
             </section>
         )
