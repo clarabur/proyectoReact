81ab973a26595ba46cf8ae9f183f63b9d44ae2e7
agoscervio <acervio@hotmail.com>
2021-09-13 10:12:22 -0300

    css footer

--- proyecto-react/src/components/Footer/Footer.js
+++ proyecto-react/src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from "react"
+import "./footer.css"
 
 function Footer(){
     return(
-        <ul class="team">
-            <li>Clara Bur</li>
-            <li>Victoria Villarreal</li>
-            <li>Agostina Cervio</li>
+        <ul className="team">
+            <li className="cadaTeam">Clara Bur</li>
+            <li className="cadaTeam">Victoria Villarreal</li>
+            <li className="cadaTeam">Agostina Cervio</li>
         </ul>
     )
 }
