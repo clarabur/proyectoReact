34475cfce33b5d201776ce5d1930b0d0da92ae29
Victoria Villarreal <victoriavillarreal@MacBook-Pro-de-Victoria.local>
2021-09-19 16:54:31 -0300

    -

--- proyecto-react/src/components/Card/Card.js
+++ proyecto-react/src/components/Card/Card.js
@@ -26,6 +26,7 @@ class Card extends Component {
             })
         }
     }
+
     sombreado(){
        this.setState({
            sombra: true
@@ -33,55 +34,53 @@ class Card extends Component {
        console.log(this.state.sombra)
 
     }
+
     normal(){
         this.setState({
             sombra: false
         })
     }
+
     zoom(){
         this.setState({
             zoom: true
         })
-        
- 
-     }
+    }
+
      zoomOut(){
          this.setState({
-             zoom: false
-         })
-     }
+            zoom: false
+        })
+    }
 
     render(){
         return(
 
             <article className={` ${this.state.sombra? 'sombraCard' : ' '} ${this.props.valor? 'card' : 'cardColumn'}  `} onMouseOver={()=>this.sombreado()} onMouseOut={()=>this.normal()} >
-            <section className={` ${this.props.valor?'navigation' : 'navigationColumn'}`}>
-                <div className='botonesArribaTarjeta'>
-                    <i className="fas fa-chevron-left"></i>
-                    <i className="fas fa-chevron-right"></i>
-                    <i className="far fa-window-close eliminarr" onClick={() => this.props.remove(this.props.dataMovie.id)}></i>
-                </div>
-                
-            </section>
-            <main className={` ${this.props.valor?'imagen-info' : 'imagenColumn'}`}>
-                <div onMouseOut={()=>this.zoomOut()} onMouseOver={()=>this.zoom()} className={` ${this.props.valor?'contenedor-imagen' : 'contenedor-imagenColumn'} ${this.state.zoom ? 'zoomImage' : ''}`}>
-                    <img className={` ${this.props.valor?'imagenes' : 'imagenesColumna'}`} src={`https://image.tmdb.org/t/p/w342${this.props.dataMovie.poster_path}`} alt=""/>
-                </div>
-                <div className={` ${this.props.valor?'contenedor-info' : 'contenedorInfoColumn'}`}>
-                    <h3 className='titulo'>{this.props.dataMovie.title}</h3>
-                    <p className="description">{this.props.dataMovie.overview}</p>
-                    <section className="aditional-info">
-                        <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Fecha de lanzamiento: {this.props.dataMovie.release_date}</p>
-                        <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Título original: {this.props.dataMovie.original_title}</p>
-                        <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Lenguaje original: {this.props.dataMovie.original_language}</p>
-                        <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Popularidad: {this.props.dataMovie.popularity}</p>
-                        <p className='more' onClick={() => this.viewMore()}>{this.state.text}</p>
-                    </section>
-                </div>
-            </main>
-        </article>
-
-
+                <section className={` ${this.props.valor?'navigation' : 'navigationColumn'}`}>
+                    <div className='botonesArribaTarjeta'>
+                        <i className="fas fa-chevron-left"></i>
+                        <i className="fas fa-chevron-right"></i>
+                        <i className="far fa-window-close eliminarr" onClick={() => this.props.remove(this.props.dataMovie.id)}></i>
+                    </div>  
+                </section>
+                <main className={` ${this.props.valor?'imagen-info' : 'imagenColumn'}`}>
+                    <div onMouseOut={()=>this.zoomOut()} onMouseOver={()=>this.zoom()} className={` ${this.props.valor?'contenedor-imagen' : 'contenedor-imagenColumn'} ${this.state.zoom ? 'zoomImage' : ''}`}>
+                        <img className={` ${this.props.valor?'imagenes' : 'imagenesColumna'}`} src={`https://image.tmdb.org/t/p/w342${this.props.dataMovie.poster_path}`} alt=""/>
+                    </div>
+                    <div className={` ${this.props.valor?'contenedor-info' : 'contenedorInfoColumn'}`}>
+                        <h3 className='titulo'>{this.props.dataMovie.title}</h3>
+                        <p className="description">{this.props.dataMovie.overview}</p>
+                        <section className="aditional-info">
+                            <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Fecha de lanzamiento: {this.props.dataMovie.release_date}</p>
+                            <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Título original: {this.props.dataMovie.original_title}</p>
+                            <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Lenguaje original: {this.props.dataMovie.original_language}</p>
+                            <p className={`extra ${this.state.viewMore ? 'show' : 'hide'}`}>Popularidad: {this.props.dataMovie.popularity}</p>
+                            <p className='more' onClick={() => this.viewMore()}>{this.state.text}</p>
+                        </section>
+                    </div>
+                </main>
+            </article>
 
         )
     }
